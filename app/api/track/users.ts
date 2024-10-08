84aedb243a1e0e8088ebafd0c1c00733c92b642b
EightRay <50017356+EightRay@users.noreply.github.com>
2024-10-08 22:55:54 +0800

    Added users.ts

---
+++ app/api/track/users.ts
@@ -0,0 +1,13 @@
+// follow the structure bellow for each entry to add users
+export const users = [
+    {
+        id: "id here",
+        name:"user name here",
+        hash: "public key hash here"
+    },
+    {
+        id: "id here",
+        name:"user name here",
+        hash: "public key hash here"
+    }
+]
